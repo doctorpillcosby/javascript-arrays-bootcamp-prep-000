0e179c76993e54e2c1bb85005e3e1f7e42e453c1
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-29 09:50:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocoloateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
   var array = "foo"
