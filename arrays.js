a865e005c641e7f9ab476b320ef1d437a3a5d562
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-29 09:46:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,44 +1,5 @@
 var chocoloateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  
-var array = "foo";
-var element = 1;
-  return [array, element]
-}
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  
-  array.unshift(element)
-  
-  return array
-  
-  }
-  
-function accessElementInArray(array)
-  { return array[2]
-    
-    
-  }
-  
-  function destructivelyRemoveElementFromBeginningOfArray(array)
-  {
-    array.shift(array)
-    
-    return array
-  }
-  
-  function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
-}
-
-function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
-  return array
-}
-
-function accessElementInArray(array, index) {
-  return array[index]
-}
   
   
