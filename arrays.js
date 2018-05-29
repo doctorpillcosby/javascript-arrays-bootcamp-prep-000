ce1eb9a2a0b2e4e8dbb4d20628b472b7c1d67e1b
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-05-29 09:52:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
 function addElementToEndOfArray(array, element){
   array.push(element)
   return array
